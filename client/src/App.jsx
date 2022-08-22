141a7c81c626db3221f436f8d701694d18666833
Aufa Fauqi Ardhiqi <79738424+ardhiqii@users.noreply.github.com>
2022-08-22 16:57:12 +0700

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import {Routes, Route} from "react-router-dom"
+function App() {
+
+  return (
+    <div className="App">
+      <Routes>
+        <h1>
+        <a href=""></a>          
+        </h1>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
